4cc0f948d2bef00b5bbf8f7ab0a019a5f91f3e61
ryangalea23 <ryan.galea@voicefriend.net>
2024-01-15 16:49:45 -0500

    test

--- dashboard1/src/App.js
+++ dashboard1/src/App.js
@@ -14,7 +14,7 @@ import './App.css';
 
       useEffect(() => {
         const receiveMessage = (event) => {
-          if (event.origin !== 'http://expected-origin.com') {
+          if (event.origin !== 'https://api.goicon.com') {
             return;
           }
 
@@ -22,7 +22,7 @@ import './App.css';
             console.log("Received URL: ", event.data.url);
             const extractedFacilityNumber = getFacilityNumber(event.data.url);
             console.log("Facility Number: ", extractedFacilityNumber);
-            setFacilityNumber(extractedFacilityNumber); // Update state with the extracted facility number
+            setFacilityNumber("998"); // Update state with the extracted facility number extractedFacilityNumber
           }
         };
 
@@ -33,97 +33,71 @@ import './App.css';
         };
       }, []);
 
+const handleButtonClick = (path) => {
+  // Construct the full URL by concatenating the master app's URL and the relative path
+  const fullURL = 'https://api.goicon.com' + path;
+  
+  // Update the URL within the iframe
+  window.location.href = fullURL;
+};
+
+      
+    
 
       return (
         <div className="App">
-          <h2 className="sonida_layout_color--tertiary">
-            <img src="https://api.goicon.com/social/editors/download/sp_logo_reverse_modern_complete1-xZeDorezeW-1704244572926.png" alt="Logo" className="logo"/>
-            <em>Resident Portal</em>
-          </h2>
-      
-          <table className="centered">
-            <tbody>
-              <tr>
-                <td>
-                  <a href={`https://api.goicon.com/facilities/${facilityNumber}/social/announcements`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/announcement/v6/24px.svg?download=true" alt="Announcement Icon" className="icon-style"/>
-                      <br /> Announcements
-                    </button>
-                  </a>
-                </td>
-                <td>
-                  <a href={`https://api.goicon.com/facilities/${facilityNumber}/social/pages/14478`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/login/v1/24px.svg?download=true" alt="Check-In Icon" className="icon-style"/>
-                      <br /> Check-In
-                    </button>
-                  </a>
-                </td>
-                <td>
-                  <a href={`https://api.goicon.com/facilities/${facilityNumber}/social/events/calendar/`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/event_note/v1/24px.svg?download=true" alt="Calendar Icon" className="icon-style"/>
-                      <br /> Calendar
-                    </button>
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <a href={`https://api.goicon.com/facilities/${facilityNumber}/social/content-library`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/article/v9/24px.svg?download=true" alt="Content Icon" className="icon-style"/>
-                      <br /> Content
-                    </button>
-                  </a>
-                </td>
-                <td>
-                  <a href={`https://api.goicon.com/facilities/${facilityNumber}/social/reservation-forms`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/event/v1/24px.svg?download=true" alt="Reservations Icon" className="icon-style"/>
-                      <br /> Reservations
-                    </button>
-                  </a>
-                </td>
-                <td>
-                  <a href={`https://api.goicon.com/facilities/${facilityNumber}/social/users`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/contact_phone/v1/24px.svg?download=true" alt="Directory Icon" className="icon-style"/>
-                      <br /> Directory
-                    </button>
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <a href={`https://api.goicon.com/facilities/${facilityNumber}/social/groups`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/group/v1/24px.svg?download=true" alt="Groups Icon" className="icon-style"/>
-                      <br /> Groups
-                    </button>
-                  </a>
-                </td>
-                <td>
-                  <a href={`https://api.goicon.com/facilities/${facilityNumber}/social/worxhub`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/build/v1/24px.svg?download=true" alt="Maintenance Icon" className="icon-style"/>
-                      <br /> Maintenance
-                    </button>
-                  </a>
-                </td>
-                <td>
-                  <a href={`https://api.caremerge.com/facilities/${facilityNumber}/social/pages/6233`} className="button-link">
-                    <button className="button-style">
-                      <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/directions_bus/v1/24px.svg?download=true" alt="Bus Icon" className="icon-style"/>
-                      <br /> Transportation
-                    </button>
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="header-container">
+          <img src="https://www.shellpoint.org/wp-content/uploads/2022/11/sp_logo.png" alt="Logo" className="logo"/>
+          <em className="subheader">Resident Portal</em>
         </div>
+  
+        <div className="button-container">
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/announcements`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/announcement/v6/24px.svg?download=true" alt="Announcement Icon" className="icon-style"/>
+            <br /> Announcements
+          </button>
+  
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/check-ins`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/login/v1/24px.svg?download=true" alt="Check-In Icon" className="icon-style"/>
+            <br /> Check-In
+          </button>
+  
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/events/calendar/`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/event_note/v1/24px.svg?download=true" alt="Calendar Icon" className="icon-style"/>
+            <br /> Calendar
+          </button>
+  
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/content-library`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/article/v9/24px.svg?download=true" alt="Content Icon" className="icon-style"/>
+            <br /> Content
+          </button>
+  
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/reservation-forms`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/event/v1/24px.svg?download=true" alt="Reservations Icon" className="icon-style"/>
+            <br /> Reservations
+          </button>
+  
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/users`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/contact_phone/v1/24px.svg?download=true" alt="Directory Icon" className="icon-style"/>
+            <br /> Directory
+          </button>
+  
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/groups`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/group/v1/24px.svg?download=true" alt="Groups Icon" className="icon-style"/>
+            <br /> Groups
+          </button>
+  
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/worxhub`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/build/v1/24px.svg?download=true" alt="Maintenance Icon" className="icon-style"/>
+            <br /> Maintenance
+          </button>
+  
+          <button className="button-style" onClick={() => handleButtonClick(`/facilities/998/social/forms`)}>
+            <img src="https://fonts.gstatic.com/s/i/materialiconsoutlined/directions_bus/v1/24px.svg?download=true" alt="Bus Icon" className="icon-style"/>
+            <br /> Forms & Requests
+          </button>
+        </div>
+      </div>
       );
       
 }
